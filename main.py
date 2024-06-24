f22d1c809106c4ec85ffd31cd1cd4e0e89a3b360
Tatiana <58085046+tdlead@users.noreply.github.com>
2024-06-24 13:37:51 +0200

    - add reading/writing of to do list to file

--- main.py
+++ main.py
@@ -8,25 +8,54 @@ I learnt/remembered:
  - enumerate function make available not only the items but also the indexes of the list
 """
 
-todos = []
+file_name = 'todos.txt'
+
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        data = file.readlines()
+    return data
+
+
+def write_file(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+
+def print_content(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for index, element in enumerate(lines):
+            print(f'{index + 1} : {element.strip()}')
+
 
 while True:
     user_action = input("Do you want to add, show or exit?")
+
     match user_action:
         case 'add':
             todo = input("Enter to do: ")
-            todos.append(todo.title())
+            todo = todo.title() + '\n'
+
+            todos = read_file(file_name)
+            todos.append(todo)
+            write_file(file_name, todos)
+
         case 'show' | 'display':
-            for index, element in enumerate(todos):
-                print(f'{index + 1} : {element}')
+            print_content(file_name)
+
         case 'edit':
-            edit_todo = int(input("What number do you want to edit?"))
-            new_todo = input("Enter new value: ")
-            todos[edit_todo - 1] = new_todo
+            edit_todo = int(input("What number do you want to edit?")) - 1
+            new_todo = input("Enter new value: ").title() + '\n'
+            todos = read_file(file_name)
+            todos[edit_todo] = new_todo
+            write_file(file_name, todos)
+
         case 'complete':
             complete_item = int(input("What number do you want to complete?"))
+            todos = read_file(file_name)
             completed_item = todos.pop(complete_item - 1)
-            print(f'You rock! "{completed_item}" Task was completed!')
+            write_file(file_name, todos)
+            print(f'You rock! "{completed_item.strip()}" Task was completed!')
         case 'exit':
             break
-
